8a8d6ada27cb0aa0490d498d3b9db6f77a3f76ba
roylee0912 <leeroy0912@gmail.com>
2024-03-08 16:58:34 -0800

    done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int main()
 {
-   
-   /* Complete your code */
+   int N;
+    cout << "Enter the number of rows: ";
+    cin >> N;
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j <= i; j++) {
+            cout << char('A' + j) << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+
   
 }
